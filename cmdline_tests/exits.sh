2b42e0d6c427e2153a44b8956c3e19f06d2acf1e
Jintack Lim <jintack@cs.columbia.edu>
2017-03-31 10:49:09 -0500

    Prepare to extend to get exit stats from L1 as well as L0
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/exits.sh
+++ cmdline_tests/exits.sh
@@ -1,40 +1,40 @@
 APP=${1:-"noapp"}
 ALL_EXITS=$APP-exits-all.txt
 
-PREV_EXIT=0
-PREV_IO_EXIT=0
-PREV_IRQ_EXIT=0
-PREV_HALT_EXIT=0
-PREV_MMIO_EXIT=0
-PREV_VMCS_EXIT=0
-PREV_MSR_EXIT=0
+declare -a PREV_EXIT
+declare -a PREV_IO_EXIT
+declare -a PREV_IRQ_EXIT
+declare -a PREV_HALT_EXIT
+declare -a PREV_MMIO_EXIT
+declare -a PREV_VMCS_EXIT
+declare -a PREV_MSR_EXIT
 
-CURR_EXIT=0
-CURR_IO_EXIT=0
-CURR_IRQ_EXIT=0
-CURR_HALT_EXIT=0
-CURR_MMIO_EXIT=0
-CURR_VMCS_EXIT=0
-CURR_MSR_EXIT=0
+declare -a CURR_EXIT
+declare -a CURR_IO_EXIT
+declare -a CURR_IRQ_EXIT
+declare -a CURR_HALT_EXIT
+declare -a CURR_MMIO_EXIT
+declare -a CURR_VMCS_EXIT
+declare -a CURR_MSR_EXIT
 
 function save_prev {
-	PREV_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/exits')
-	PREV_IO_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/io_exits')
-	PREV_IRQ_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
-	PREV_HALT_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
-	PREV_MMIO_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
-	PREV_VMCS_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/vmcs')
-	PREV_MSR_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/msr')
+	PREV_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/exits')
+	PREV_IO_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/io_exits')
+	PREV_IRQ_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
+	PREV_HALT_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
+	PREV_MMIO_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
+	PREV_VMCS_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/vmcs')
+	PREV_MSR_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/msr')
 }
 
 function save_curr {
-	CURR_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/exits')
-	CURR_IO_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/io_exits')
-	CURR_IRQ_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
-	CURR_HALT_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
-	CURR_MMIO_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
-	CURR_VMCS_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/vmcs')
-	CURR_MSR_EXIT=$(ssh jintackl@10.10.1.2 'sudo cat /sys/kernel/debug/kvm/msr')
+	CURR_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/exits')
+	CURR_IO_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/io_exits')
+	CURR_IRQ_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
+	CURR_HALT_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
+	CURR_MMIO_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
+	CURR_VMCS_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/vmcs')
+	CURR_MSR_EXIT[$1]=$(ssh jintackl@$2 'sudo cat /sys/kernel/debug/kvm/msr')
 }
 
 function print_title {
@@ -42,13 +42,13 @@ function print_title {
 }
 
 function save_diff {
-	N_IO_EXIT=$((CURR_IO_EXIT - PREV_IO_EXIT))
-	N_IRQ_EXIT=$((CURR_IRQ_EXIT - PREV_IRQ_EXIT))
-	N_HALT_EXIT=$((CURR_HALT_EXIT - PREV_HALT_EXIT))
-	N_MMIO_EXIT=$((CURR_MMIO_EXIT - PREV_MMIO_EXIT))
-	N_VMCS_EXIT=$((CURR_VMCS_EXIT - PREV_VMCS_EXIT))
-	N_MSR_EXIT=$((CURR_MSR_EXIT - PREV_MSR_EXIT))
-	N_EXIT=$((CURR_EXIT - PREV_EXIT))
+	N_IO_EXIT=$((CURR_IO_EXIT[$1] - PREV_IO_EXIT[$1]))
+	N_IRQ_EXIT=$((CURR_IRQ_EXIT[$1] - PREV_IRQ_EXIT[$1]))
+	N_HALT_EXIT=$((CURR_HALT_EXIT[$1] - PREV_HALT_EXIT[$1]))
+	N_MMIO_EXIT=$((CURR_MMIO_EXIT[$1] - PREV_MMIO_EXIT[$1]))
+	N_VMCS_EXIT=$((CURR_VMCS_EXIT[$1] - PREV_VMCS_EXIT[$1]))
+	N_MSR_EXIT=$((CURR_MSR_EXIT[$1] - PREV_MSR_EXIT[$1]))
+	N_EXIT=$((CURR_EXIT[$1] - PREV_EXIT[$1]))
 	echo "Number of exits: $N_EXIT"
 
 	echo $N_EXIT,$N_IO_EXIT,$N_IRQ_EXIT,$N_HALT_EXIT,$N_MMIO_EXIT,$N_VMCS_EXIT,$N_MSR_EXIT >>$ALL_EXITS
--- cmdline_tests/memcached.sh
+++ cmdline_tests/memcached.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 SERVER=${1-127.0.0.1}
-REPTS=${2-40}
+REPTS=${2-1}
 RESULTS=memcached.txt
 source exits.sh memcached
 
 echo "Benchmarking $SERVER" | tee >(cat >> $RESULTS)
-echo "Benchmarking $SERVER" >> $EXITS
+echo "Benchmarking $SERVER" >> $ALL_EXITS
 print_title
 
 for i in `seq 1 $REPTS`; do
-	save_prev
+	save_prev 0 10.10.1.2
 	memtier_benchmark -p 11211 -P memcache_binary -s $SERVER 2>&1 | \
 		tee >(grep 'Totals' | awk '{ print $2 }' >> $RESULTS)
-	save_curr
-	save_diff
+	save_curr 0 10.10.1.2
+	save_diff 0
 
 done
