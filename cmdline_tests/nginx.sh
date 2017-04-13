fdd9d25005078f27873a9ec7fb7c13882a272c69
Jintack Lim <jintack@cs.columbia.edu>
2017-04-13 10:46:04 -0500

    Set nginx concurrent level to 4
    VHE guest can run 4, but not 10
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/nginx.sh
+++ cmdline_tests/nginx.sh
@@ -9,7 +9,7 @@ echo "Measuring performance of $SRV"
 RESULTS=nginx.txt
 TMP=/tmp/nginx.txt
 
-CMD="sudo siege -q -t 10s -c 100  http://$1/gcc/index.html -b"
+CMD="sudo siege -q -t 10s -c 4  http://$1/gcc/index.html -b"
 for i in `seq 1 $REPTS`; do
 	$CMD 2>&1 | tee /tmp/nginx.txt
 	cat $TMP | grep 'Transaction rate' | awk '{ print $3 }' >> $RESULTS
