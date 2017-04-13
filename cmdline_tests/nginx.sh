08d494dec7d384f1712691f0fd9f720c24ebb457
Jintack Lim <jintack@cs.columbia.edu>
2017-04-13 08:20:03 -0600

    Check if transactions are successful

--- cmdline_tests/nginx.sh
+++ cmdline_tests/nginx.sh
@@ -7,10 +7,17 @@ REPTS=${2-50}
 echo "Measuring performance of $SRV"
 
 RESULTS=nginx.txt
+TMP=/tmp/nginx.txt
 
-CMD="siege -q -t 10s -c 100  http://$1/gcc/index.html -b"
+CMD="sudo siege -q -t 10s -c 100  http://$1/gcc/index.html -b"
 for i in `seq 1 $REPTS`; do
-	$CMD 2>&1 | tee >(grep 'Transaction rate' | awk '{ print $3 }' >> $RESULTS)
+	$CMD 2>&1 | tee /tmp/nginx.txt
+	cat $TMP | grep 'Transaction rate' | awk '{ print $3 }' >> $RESULTS
+	SUCCESS=`cat $TMP | grep 'Successful' | awk '{ print $3 }'`
+	if [[ "$SUCCESS" == "0" ]]; then
+		echo "No successful transaction."
+		exit 1
+	fi
 done
 
 
