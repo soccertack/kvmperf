7135456d8d20de488df74c8aa9ed39adf5083b9d
Jintack Lim <jintack@cs.columbia.edu>
2017-04-13 10:45:31 -0500

    Make nginx servie start without an error

--- cmdline_tests/nginx_install.sh
+++ cmdline_tests/nginx_install.sh
@@ -4,6 +4,7 @@ which nginx > /dev/null
 if [[ $? != 0 ]]; then
 	apt-get install -y nginx 
 	update-rc.d nginx disable
+	sed -i '/ipv6only/d' /etc/nginx/sites-available/default
 fi
 
 which siege > /dev/null
