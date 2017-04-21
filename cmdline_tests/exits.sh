43f3a2a85a022608abacb1813f12fd7f2a831b2e
Jintack Lim <jintack@cs.columbia.edu>
2017-04-20 21:05:24 -0600

    Add ipi exit counts
    Add iodeventfd as well, but that partially belongs to mmio-kernel.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/exits.sh
+++ cmdline_tests/exits.sh
@@ -7,6 +7,8 @@ declare -a PREV_WFI_EXIT
 declare -a PREV_WFE_EXIT
 declare -a PREV_IO_USER_EXIT
 declare -a PREV_HVC_EXIT
+declare -a PREV_IPI_EXIT
+declare -a PREV_IO_EXIT
 
 declare -a CURR_EXIT
 declare -a CURR_IO_KERNEL_EXIT
@@ -14,6 +16,8 @@ declare -a CURR_WFI_EXIT
 declare -a CURR_WFE_EXIT
 declare -a CURR_IO_USER_EXIT
 declare -a CURR_HVC_EXIT
+declare -a CURR_IPI_EXIT
+declare -a CURR_IO_EXIT
 
 function save_prev {
 	PREV_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/exits')
@@ -22,6 +26,8 @@ function save_prev {
 	PREV_WFE_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/wfe_exit_stat')
 	PREV_IO_USER_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exit_user')
 	PREV_HVC_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/hvc_exit_stat')
+	PREV_IPI_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/ipi_exit_stat')
+	PREV_IO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/ioeventfd_exit_stat')
 }
 
 function save_curr {
@@ -31,10 +37,12 @@ function save_curr {
 	CURR_WFE_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/wfe_exit_stat')
 	CURR_IO_USER_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exit_user')
 	CURR_HVC_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/hvc_exit_stat')
+	CURR_IPI_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/ipi_exit_stat')
+	CURR_IO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/ioeventfd_exit_stat')
 }
 
 function print_title {
-	echo "total,io_kernel,io_user,wfi,wfe,hvc" >> $ALL_EXITS
+	echo "total,io_kernel,dummy,ipi,io_user,wfi,wfe,hvc" >> $ALL_EXITS
 }
 
 function save_diff {
@@ -43,8 +51,10 @@ function save_diff {
 	N_WFI_EXIT=$((CURR_WFI_EXIT[$1] - PREV_WFI_EXIT[$1]))
 	N_WFE_EXIT=$((CURR_WFE_EXIT[$1] - PREV_WFE_EXIT[$1]))
 	N_HVC_EXIT=$((CURR_HVC_EXIT[$1] - PREV_HVC_EXIT[$1]))
+	N_IPI_EXIT=$((CURR_IPI_EXIT[$1] - PREV_IPI_EXIT[$1]))
+	N_IO_EXIT=$((CURR_IO_EXIT[$1] - PREV_IO_EXIT[$1]))
 	N_EXIT=$((CURR_EXIT[$1] - PREV_EXIT[$1]))
 	echo "Number of exits: $N_EXIT"
 
-	echo $N_EXIT,$N_IO_KERNEL_EXIT,$N_IO_USER_EXIT,$N_WFI_EXIT,$N_WFE_EXIT,$N_HVC_EXIT >>$ALL_EXITS
+	echo $N_EXIT,$N_IO_KERNEL_EXIT,$N_IO_EXIT,$N_IPI_EXIT,$N_IO_USER_EXIT,$N_WFI_EXIT,$N_WFE_EXIT,$N_HVC_EXIT >>$ALL_EXITS
 }
