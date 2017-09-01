79bd07c7cd4781c383c02c02091b87f497898a9d
Jintack Lim <jintack@cs.columbia.edu>
2017-09-01 04:32:59 -0600

    Repeat experiment without pexpect.
    pexpect resulted in 100% cpu util.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

---
+++ cmdline_tests/client.py
@@ -0,0 +1,60 @@
+import pexpect
+import sys
+import os
+import datetime
+import time
+import socket
+
+FULL_TH = 50000
+ONE_MORE_TH = 40000
+
+def read_last_line(filename):
+	fileHandle = open ( filename ,"r" )
+	lineList = fileHandle.readlines()
+	fileHandle.close()
+	return float(lineList[-1].rstrip())
+
+def run_memcached(n_run, out_filename = "memcached.txt"):
+	os.system('ssh root@10.10.1.101 "sudo service memcached start"')
+	os.system("./memcached.sh 10.10.1.101 %d %s" % (n_run, out_filename))
+
+	time.sleep(1)
+	val = read_last_line(out_filename)
+
+	print ("running memcached")
+	return int(val)
+
+def run_full(out_filename):
+	for x in range(0, 5):
+		name = out_filename+"-"+str(x)
+		run_memcached(40, name)
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('10.10.1.2', 8889))
+
+i = 0
+while 1:
+	i += 1
+
+	score = run_memcached(1)
+
+	if score < FULL_TH and score > ONE_MORE_TH:
+		print ("second chance")
+		score = run_memcached(1)
+
+	if score > FULL_TH:
+		run_full("memcached-%d"%i)
+	else:
+		print ("score is %d. Restart..%dth" % (score, i))
+		clientsocket.send('reboot')
+
+		while True:
+			buf = clientsocket.recv(64)
+			if len(buf) > 0:
+				print buf
+				if buf == "ready":
+					print "ready is received"
+					break
+
+
+
