bedaac9fdc40188f11061c5809f6c1eac0de8c60
Jintack Lim <jintack@cs.columbia.edu>
2018-04-13 23:25:04 -0500

    Use server.py for single level
    We also try to kill VM gently since x86 VMs are very vulnerable.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/server.py
+++ cmdline_tests/server.py
@@ -9,12 +9,13 @@ import socket
 import argparse
 
 LOCAL_SOCKET = 8890
-def pin_vcpus():
+def pin_vcpus(level):
 	os.system('cd /srv/vm/qemu/scripts/qmp/ && sudo ./pin_vcpus.sh && cd -')
-	os.system('ssh root@10.10.1.100 "cd vm/qemu/scripts/qmp/ && ./pin_vcpus.sh"')
+	if level > 1:
+		os.system('ssh root@10.10.1.100 "cd vm/qemu/scripts/qmp/ && ./pin_vcpus.sh"')
 	print ("vcpu is pinned")
 
-def boot_nvm(pvpassthrough):
+def boot_nvm(pvpassthrough, level):
 
 	if pvpassthrough:
 		child.sendline('cd /srv/vm && ./run-guest-smmu.sh')
@@ -23,32 +24,40 @@ def boot_nvm(pvpassthrough):
 
         child.expect('L1.*$')
 
-	if pvpassthrough:
-		child.sendline('cd ~/vm && ./run-guest-vfio.sh')
-	else:
-		child.sendline('cd ~/vm && ./run-guest.sh')
+	if level > 1:
+		if pvpassthrough:
+			child.sendline('cd ~/vm && ./run-guest-vfio.sh')
+		else:
+			child.sendline('cd ~/vm && ./run-guest.sh')
 
-        child.expect('L2.*$')
+		child.expect('L2.*$')
+
 	time.sleep(2)
-	pin_vcpus()
+	pin_vcpus(level)
 	time.sleep(2)
 
-def reboot(pvpassthrough):
+def reboot(pvpassthrough, level):
 	
-	# Kill VM. (we may do halt -p)
-	os.system('pgrep qemu | xargs sudo kill -9')
-	time.sleep(2)
+	if level > 1:
+		os.system('ssh root@10.10.1.101 "halt -p"')
+		child.expect('L1.*$')
 
+	os.system('ssh root@10.10.1.100 "halt -p"')
 	child.expect('kvm-node.*')
-	boot_nvm(pvpassthrough)
+	boot_nvm(pvpassthrough, level)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--pvpassthrough", help="enable pv-passthrough", action='store_true')
+parser.add_argument("-l", "--level", help="set virtualization level")
 args = parser.parse_args()
 
 pvpassthrough = False
 if args.pvpassthrough:
 	pvpassthrough = True
+
+level = 2
+if args.level:
+	level = int(args.level)
 
 child = pexpect.spawn('bash')
 child.logfile = sys.stdout
@@ -56,7 +65,7 @@ child.timeout=None
 
 child.sendline('')
 child.expect('kvm-node.*')
-boot_nvm(pvpassthrough)
+boot_nvm(pvpassthrough, level)
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', 8889))
@@ -69,5 +78,5 @@ while True:
     if len(buf) > 0:
         print buf
 	if buf == "reboot":
-		reboot(pvpassthrough)
+		reboot(pvpassthrough, level)
 		connection.send("ready")
