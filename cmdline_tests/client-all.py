90f9ab272c852ef7b364518df73c22705189f9c9
Jintack Lim <jintack@cs.columbia.edu>
2018-04-14 00:11:09 -0500

    Make client-all script to run for L1
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/client-all.py
+++ cmdline_tests/client-all.py
@@ -8,16 +8,19 @@ import socket
 from datetime import datetime
 import argparse
 
-def run_mysql():
-	os.system("./mysql.sh run 10.10.1.101 root /root/kvmperf/cmdline_tests")
+L1_IP="10.10.1.100"
+L2_IP="10.10.1.101"
 
-def run_nginx():
-	os.system('ssh root@10.10.1.101 "sudo service nginx start"')
+def run_mysql(ip_addr):
+	os.system("./mysql.sh run %s root /root/kvmperf/cmdline_tests" % ip_addr)
+
+def run_nginx(ip_addr):
+	os.system('ssh root@%s "sudo service nginx start"' % ip_addr)
 	time.sleep(1)
-	os.system("./nginx.sh 10.10.1.101")
-	os.system('ssh root@10.10.1.101 "sudo service nginx stop"')
+	os.system("./nginx.sh %s" % ip_addr)
+	os.system('ssh root@%s "sudo service nginx stop"' % ip_addr)
 
-def run_netperf(name):
+def run_netperf(name, ip_addr):
 
 	filename = "netperf-" + name + ".txt"
 	if name == "stream":
@@ -29,48 +32,48 @@ def run_netperf(name):
 	else:
 		return
 	
-	os.system('ssh root@10.10.1.101 "sudo service netperf start"')
+	os.system('ssh root@%s "sudo service netperf start"' % ip_addr)
 	time.sleep(1)
-	os.system("./netperf.sh 10.10.1.101 %s %s" % (testname, filename))
-	os.system('ssh root@10.10.1.101 "sudo service netperf stop"')
+	os.system("./netperf.sh %s %s %s" % (ip_addr, testname, filename))
+	os.system('ssh root@%s "sudo service netperf stop"' % ip_addr)
 
-def run_stream():
-	run_netperf("stream")
+def run_stream(ip_addr):
+	run_netperf("stream", ip_addr)
 
-def run_maerts():
-	run_netperf("maerts")
+def run_maerts(ip_addr):
+	run_netperf("maerts", ip_addr)
 
-def run_rr():
-	run_netperf("rr")
+def run_rr(ip_addr):
+	run_netperf("rr", ip_addr)
 
-def run_apache():
-	os.system('ssh root@10.10.1.101 "sudo service apache2 start"')
+def run_apache(ip_addr):
+	os.system('ssh root@%s "sudo service apache2 start"' % ip_addr)
 	time.sleep(1)
-	os.system("./apache.sh 10.10.1.101")
-	os.system('ssh root@10.10.1.101 "sudo service apache2 stop"')
+	os.system("./apache.sh %s" % ip_addr)
+	os.system('ssh root@%s "sudo service apache2 stop"' % ip_addr)
 
-def run_memcached(n_run = 40, out_filename = "memcached.txt"):
-	os.system('ssh root@10.10.1.101 "sudo service memcached start"')
+def run_memcached(ip_addr, n_run = 40, out_filename = "memcached.txt"):
+	os.system('ssh root@%s "sudo service memcached start"' % ip_addr)
 	time.sleep(1)
-	os.system("./memcached.sh 10.10.1.101 %d %s" % (n_run, out_filename))
-	os.system('ssh root@10.10.1.101 "sudo service memcached stop"')
+	os.system("./memcached.sh %s %d %s" % (ip_addr, n_run, out_filename))
+	os.system('ssh root@%s "sudo service memcached stop"' % ip_addr)
 
 def move_data(experiment_name, i):
 	os.system("mkdir -p %s/%d" % (experiment_name, i))
 	os.system("mv *.txt %s/%d" % (experiment_name, i))
 	return
 
-def run_tests():
+def run_tests(ip_addr):
 	print("start a test run")
 	print(str(datetime.now()))
 
-	run_mysql()
-	run_stream()
-	run_maerts()
-	run_rr()
-	run_memcached()
-	run_apache()
-	run_nginx()
+	run_mysql(ip_addr)
+	run_stream(ip_addr)
+	run_maerts(ip_addr)
+	run_rr(ip_addr)
+	run_memcached(ip_addr)
+	run_apache(ip_addr)
+	run_nginx(ip_addr)
 
 	print("end a test run")
 	print(str(datetime.now()))
@@ -85,6 +88,7 @@ def connect_to_server():
 
 def main():
 	parser = argparse.ArgumentParser()
+	parser.add_argument("-l", "--level", help="virt level")
 	parser.add_argument("-n", "--name", help="setup experiment name")
 	parser.add_argument("-i", "--iterations", help="setup per-reboot iterations")
 	parser.add_argument("-r", "--reboot", help="setup number of reboot")
@@ -102,6 +106,12 @@ def main():
 	if args.iterations:
 		iterations = int(args.iterations)
 
+	virt_level = 2
+	ip_addr = L2_IP
+	if int(args.level) == 1:
+		virt_level = 1
+		ip_addr = L1_IP
+
 	print ('Name: %s, iterations: %d, reboot: %d' % (experiment_name, iterations, reboot))
 
 	os.system("rm *.txt")
@@ -115,7 +125,7 @@ def main():
 		start = reboot_cnt * iterations + 1
 		end = start + iterations
 		for i in range(start, end):
-			run_tests()
+			run_tests(ip_addr)
 			move_data(experiment_name, i)
 
 		if reboot <= reboot_cnt :
