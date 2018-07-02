719e641b72dd22dcbff96b5a8313ac0254531958
Jintack Lim <jintack@cs.columbia.edu>
2018-07-02 10:42:30 -0500

    Make exits.sh more extensible
    We don't need to write code to handle all exits manually. Instead, use
    for-loops and an array of exit reasons to handle them all.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/exits.sh
+++ cmdline_tests/exits.sh
@@ -1,55 +1,41 @@
 APP=${1:-"noapp"}
 ALL_EXITS=$APP-exits-all.txt
 
-declare -a PREV_EXIT
-declare -a PREV_IO_EXIT
-declare -a PREV_IRQ_EXIT
-declare -a PREV_HALT_EXIT
-declare -a PREV_MMIO_EXIT
-declare -a PREV_HANDLE_EXIT
-declare -a PREV_MSR_EXIT
+exits=(exits io_exits irq_exits halt_exits mmio_exits abc_exits msr)
 
-declare -a CURR_EXIT
-declare -a CURR_IO_EXIT
-declare -a CURR_IRQ_EXIT
-declare -a CURR_HALT_EXIT
-declare -a CURR_MMIO_EXIT
-declare -a CURR_HANDLE_EXIT
-declare -a CURR_MSR_EXIT
+for exit in ${exits[@]}; do
+	declare -a PREV_$exit
+	declare -a CURR_$exit
+done
+
+function read_raw {
+	for exit in ${exits[@]}; do
+		declare -n ref="$4_$exit"
+		ref[$1]=$(ssh $3@$2 "sudo cat /sys/kernel/debug/kvm/$exit")
+	done
+}
 
 function save_prev {
-	PREV_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/exits')
-	PREV_IO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/io_exits')
-	PREV_IRQ_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
-	PREV_HALT_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
-	PREV_MMIO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
-	PREV_HANDLE_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/abc_exits')
-	PREV_MSR_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/msr')
+	read_raw $1 $2 $3 "PREV"
 }
 
 function save_curr {
-	CURR_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/exits')
-	CURR_IO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/io_exits')
-	CURR_IRQ_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
-	CURR_HALT_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
-	CURR_MMIO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
-	CURR_HANDLE_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/abc_exits')
-	CURR_MSR_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/msr')
+	read_raw $1 $2 $3 "CURR"
 }
 
 function print_title {
-	echo "total,io,irq,halt,mmio,handle_exits,msr" >> $ALL_EXITS
+	for exit in ${exits[@]}; do
+		echo -n "$exit," >> $ALL_EXITS
+	done
+	echo "" >> $ALL_EXITS
 }
 
 function save_diff {
-	N_IO_EXIT=$((CURR_IO_EXIT[$1] - PREV_IO_EXIT[$1]))
-	N_IRQ_EXIT=$((CURR_IRQ_EXIT[$1] - PREV_IRQ_EXIT[$1]))
-	N_HALT_EXIT=$((CURR_HALT_EXIT[$1] - PREV_HALT_EXIT[$1]))
-	N_MMIO_EXIT=$((CURR_MMIO_EXIT[$1] - PREV_MMIO_EXIT[$1]))
-	N_HANDLE_EXIT=$((CURR_HANDLE_EXIT[$1] - PREV_HANDLE_EXIT[$1]))
-	N_MSR_EXIT=$((CURR_MSR_EXIT[$1] - PREV_MSR_EXIT[$1]))
-	N_EXIT=$((CURR_EXIT[$1] - PREV_EXIT[$1]))
-	echo "Number of exits: $N_EXIT"
-
-	echo $N_EXIT,$N_IO_EXIT,$N_IRQ_EXIT,$N_HALT_EXIT,$N_MMIO_EXIT,$N_HANDLE_EXIT,$N_MSR_EXIT >>$ALL_EXITS
+	for exit in ${exits[@]}; do
+		declare -n curr_ref="CURR_$exit"
+		declare -n prev_ref="PREV_$exit"
+		diff=$((curr_ref[$1] - prev_ref[$1]))
+		echo -n "$diff," >> $ALL_EXITS
+	done
+	echo "" >> $ALL_EXITS
 }
