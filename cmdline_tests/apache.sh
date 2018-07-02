386c9e692c628f149c4c0db36a1f3ede41a70150
Jintack Lim <jintack@cs.columbia.edu>
2018-07-02 08:50:54 -0500

    Use root to access L0
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/apache.sh
+++ cmdline_tests/apache.sh
@@ -16,11 +16,11 @@ print_title
 service apache2 start
 
 for i in `seq 1 $REPTS`; do
-	save_prev 0 10.10.1.2 jintackl
-	save_prev 1 10.10.1.100 root
+	save_prev 0 10.10.1.2 root
+#	save_prev 1 10.10.1.100 root
 	$CMD | tee >(grep 'Requests per second' | awk '{ print $4 }' >> $RESULTS)
-	save_curr 0 10.10.1.2 jintackl
-	save_curr 1 10.10.1.100 root
+	save_curr 0 10.10.1.2 root
+#	save_curr 1 10.10.1.100 root
 	save_diff 0
-	save_diff 1
+#	save_diff 1
 done
