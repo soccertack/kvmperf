f566a149c31cd720d1ffd618fcbafe2a83499755
Jintack Lim <jintack@cs.columbia.edu>
2018-07-02 08:51:35 -0500

    Replace VMCS exits with all handle exits.
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/exits.sh
+++ cmdline_tests/exits.sh
@@ -6,7 +6,7 @@ declare -a PREV_IO_EXIT
 declare -a PREV_IRQ_EXIT
 declare -a PREV_HALT_EXIT
 declare -a PREV_MMIO_EXIT
-declare -a PREV_VMCS_EXIT
+declare -a PREV_HANDLE_EXIT
 declare -a PREV_MSR_EXIT
 
 declare -a CURR_EXIT
@@ -14,7 +14,7 @@ declare -a CURR_IO_EXIT
 declare -a CURR_IRQ_EXIT
 declare -a CURR_HALT_EXIT
 declare -a CURR_MMIO_EXIT
-declare -a CURR_VMCS_EXIT
+declare -a CURR_HANDLE_EXIT
 declare -a CURR_MSR_EXIT
 
 function save_prev {
@@ -23,7 +23,7 @@ function save_prev {
 	PREV_IRQ_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
 	PREV_HALT_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
 	PREV_MMIO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
-	PREV_VMCS_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/vmcs')
+	PREV_HANDLE_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/abc_exits')
 	PREV_MSR_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/msr')
 }
 
@@ -33,12 +33,12 @@ function save_curr {
 	CURR_IRQ_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/irq_exits')
 	CURR_HALT_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/halt_exits')
 	CURR_MMIO_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/mmio_exits')
-	CURR_VMCS_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/vmcs')
+	CURR_HANDLE_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/abc_exits')
 	CURR_MSR_EXIT[$1]=$(ssh $3@$2 'sudo cat /sys/kernel/debug/kvm/msr')
 }
 
 function print_title {
-	echo "total,io,irq,halt,mmio,vmcs,msr" >> $ALL_EXITS
+	echo "total,io,irq,halt,mmio,handle_exits,msr" >> $ALL_EXITS
 }
 
 function save_diff {
@@ -46,10 +46,10 @@ function save_diff {
 	N_IRQ_EXIT=$((CURR_IRQ_EXIT[$1] - PREV_IRQ_EXIT[$1]))
 	N_HALT_EXIT=$((CURR_HALT_EXIT[$1] - PREV_HALT_EXIT[$1]))
 	N_MMIO_EXIT=$((CURR_MMIO_EXIT[$1] - PREV_MMIO_EXIT[$1]))
-	N_VMCS_EXIT=$((CURR_VMCS_EXIT[$1] - PREV_VMCS_EXIT[$1]))
+	N_HANDLE_EXIT=$((CURR_HANDLE_EXIT[$1] - PREV_HANDLE_EXIT[$1]))
 	N_MSR_EXIT=$((CURR_MSR_EXIT[$1] - PREV_MSR_EXIT[$1]))
 	N_EXIT=$((CURR_EXIT[$1] - PREV_EXIT[$1]))
 	echo "Number of exits: $N_EXIT"
 
-	echo $N_EXIT,$N_IO_EXIT,$N_IRQ_EXIT,$N_HALT_EXIT,$N_MMIO_EXIT,$N_VMCS_EXIT,$N_MSR_EXIT >>$ALL_EXITS
+	echo $N_EXIT,$N_IO_EXIT,$N_IRQ_EXIT,$N_HALT_EXIT,$N_MMIO_EXIT,$N_HANDLE_EXIT,$N_MSR_EXIT >>$ALL_EXITS
 }
