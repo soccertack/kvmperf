ffe97badee636ab2c019166507100d77bd9e95cd
Jintack Lim <jintack@cs.columbia.edu>
2018-08-31 15:40:23 -0500

    Install b2bua
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

---
+++ cmdline_tests/b2bua_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ "`whoami`" != "root" ]]; then
+	echo "Please run as root" >&2
+	exit 1
+fi
+
+git clone https://github.com/sobomax/libelperiodic.git
+cd libelperiodic
+./configure && make && make install
+export LD_LIBRARY_PATH=/usr/local/lib
+
+mkdir /usr/local/lib/python2.7/dist-packages/elperiodic/
+cp python/* /usr/local/lib/python2.7/dist-packages/elperiodic/
+
+cd ..
+rm -rf libelperiodic
+
+apt-get install python-dev
+pip install git+https://github.com/columbia/b2bua
