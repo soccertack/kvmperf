abd65356f4c88dce0b2488d4ec6e038baad0f98d
Jintack Lim <jintack@cs.columbia.edu>
2018-08-31 18:01:22 -0600

    Install libpcap-dev for sipp

--- cmdline_tests/sipp_install.sh
+++ cmdline_tests/sipp_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone -b v3.5.2  https://github.com/SIPp/sipp.git
-sudo apt-get -y install pkg-config libsctp-dev libgsl0-dev
+sudo apt-get -y install pkg-config libsctp-dev libgsl0-dev libpcap-dev
 cd sipp
 ./build.sh --full
 
