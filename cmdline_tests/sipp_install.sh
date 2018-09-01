36a8268304cdca1ceff239da867eadd8887d6c80
Jintack Lim <jintack@cs.columbia.edu>
2018-08-31 18:14:42 -0600

    Install more pkgs for sipp

--- cmdline_tests/sipp_install.sh
+++ cmdline_tests/sipp_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 git clone -b v3.5.2  https://github.com/SIPp/sipp.git
-sudo apt-get -y install pkg-config libsctp-dev libgsl0-dev libpcap-dev
+apt-get update
+sudo apt-get -y install pkg-config libsctp-dev libgsl0-dev libpcap-dev libssl-dev
 cd sipp
 ./build.sh --full
 
