9c33b387c9d8e63406b745a44c9486c09d909ea9
Jintack Lim <jintack@cs.columbia.edu>
2018-11-09 14:28:19 -0600

    Use import xxx instead of from xxx import *
    https://docs.python.org/3/faq/programming.html#how-do-i-share-global-variables-across-modules
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/mi_client.py
+++ cmdline_tests/mi_client.py
@@ -8,7 +8,7 @@ import socket
 from datetime import datetime
 from sk_common import *
 from mi_common import *
-from mi_api import *
+import mi_api
 
 #Client status
 C_NULL = 0
@@ -28,17 +28,18 @@ def connect_to_server():
 
 def handle_recv(c, buf):
 	global status
+
 	print buf + " is received"
 	if status == C_WAIT_FOR_BOOT_CMD:
 		if buf == MSG_BOOT:
-			child, params = init()
-			boot_nvm(child, params)
+			child, params = mi_api.init()
+			mi_api.boot_nvm(child, params)
 			c.send(MSG_BOOT_COMPLETED)
 			status = C_BOOT_COMPLETED
 	elif status == C_BOOT_COMPLETED:
 		if buf == MSG_MIGRATE:
 			print "start migration"
-			child = get_child()
+			child = mi_api.get_child()
 #			child.sendline('migrate_set_speed 4095m')
 #			child.expect('\(qemu\)')
 			child.sendline('migrate -d tcp:10.10.1.110:5555')
@@ -58,7 +59,7 @@ def handle_recv(c, buf):
 			status = C_MIGRATION_COMPLETED
 
 	if buf == MSG_TERMINATE:
-		child = get_child()
+		child = mi_api.get_child()
 		print ("Terminate VM.")
 		child.sendline('stop')
 		child.expect('\(qemu\)')
@@ -66,12 +67,11 @@ def handle_recv(c, buf):
 		child.expect('L1.*$')
 		child.sendline('h')
 		status = C_TERMINATED
-		print ("hostname: " + hostname)
-		wait_for_prompt(child, hostname)
+		mi_api.wait_for_prompt(child, mi_api.hostname)
 
 def main():
 	global status
-
+	
 	clientsocket = connect_to_server()
 	status = C_WAIT_FOR_BOOT_CMD
 
