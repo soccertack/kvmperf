d2f4c99116a6bcfa873b32731829ab060f67d267
Jintack Lim <jintack@cs.columbia.edu>
2019-08-23 17:50:35 -0500

    Move migration scripts to env repo
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- cmdline_tests/mi_common.py
+++
@@ -1,6 +0,0 @@
-MSG_BOOT = "Boot nvm"
-MSG_BOOT_COMPLETED = "Boot nvm completed"
-MSG_MIGRATE= "Start migration"
-MSG_MIGRATE_COMPLETED= "Migration completed"
-MSG_TERMINATE= "Terminate VM"
-MSG_TERMINATED= "Terminated VM"
